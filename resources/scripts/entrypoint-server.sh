e8d761279f614f871033a6b79e9fea941ff7210c
Azamov Samandar <162783893+JscorpTech@users.noreply.github.com>
2025-07-05 11:16:22 +0500

    update entrypoint-server.sh

--- resources/scripts/entrypoint-server.sh
+++ resources/scripts/entrypoint-server.sh
@@ -2,7 +2,7 @@
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
 
-gunicorn  --workers 2 -b 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker config.wsgi:application
+gunicorn  --workers 2 -b 0.0.0.0:8000 config.wsgi:application
 
 
 exit $?
